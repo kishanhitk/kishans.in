b9e6e5f130c645c01aa1fe9e7910632028c18798
kishanhitk <kishansharma1231@gmail.com>
2021-06-19 10:06:40 +0530

    💄 Add more font weights| Hey->Hi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import "@fontsource/inter/400.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/500.css";
 import theme from "@style/theme";
 
 function MyApp({ Component, pageProps }) {
