86abef184d448629d5fc0ebaa804780a36122e51
kishanhitk <kishansharma1231@gmail.com>
2021-06-20 14:15:51 +0530

    ✨ Add redirects for stroll and jums reboot

--- next.config.js
+++ next.config.js
@@ -6,6 +6,17 @@ module.exports = {
         destination: "https://blog.kishans.in/",
         permanent: false,
       },
+      {
+        source: "/stroll",
+        destination: "https://stroll-land.vercel.app/",
+        permanent: false,
+      },
+      {
+        source: "/jums-reboot",
+        destination:
+          "https://play.google.com/store/apps/details?id=com.kishans.jumsRebootFlutter",
+        permanent: false,
+      },
     ];
   },
 };
