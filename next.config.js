5d2ee2668a855304584352cce514e09a7eaf2bee
kishanhitk <kishansharma1231@gmail.com>
2021-08-14 15:16:59 +0530

    chore: Cleaner config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-
-    return config;
-  },
   async redirects() {
     return [
       {
@@ -51,5 +39,17 @@ module.exports = {
         permanent: false,
       },
     ];
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
   },
 };
