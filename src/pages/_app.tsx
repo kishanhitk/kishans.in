dd7b31931adffd2a75a1690ba8b539a7f7a816fa
kishanhitk <kishansharma1231@gmail.com>
2021-08-24 10:34:26 +0530

    feat: use lazymotion and m

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,19 +4,21 @@ import "@fontsource/inter/400.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/500.css";
 import theme from "@style/theme";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, domAnimation, LazyMotion } from "framer-motion";
 
 function MyApp({ Component, pageProps, router }) {
   const url = `https://kishans.in${router.route}`;
   return (
     <ChakraProvider theme={theme}>
-      <AnimatePresence
-        exitBeforeEnter={true}
-        initial={false}
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
-        <Component {...pageProps} key={url} />
-      </AnimatePresence>
+      <LazyMotion features={domAnimation} strict>
+        <AnimatePresence
+          exitBeforeEnter={true}
+          initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <Component {...pageProps} key={url} />
+        </AnimatePresence>
+      </LazyMotion>
     </ChakraProvider>
   );
 }
