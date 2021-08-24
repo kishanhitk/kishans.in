c1bb8e4163fba0864768eec474685e49f52eb15a
kishanhitk <kishansharma1231@gmail.com>
2021-08-24 10:56:57 +0530

    feat: use async loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,15 @@ import "@fontsource/inter/400.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/500.css";
 import theme from "@style/theme";
-import { AnimatePresence, domAnimation, LazyMotion } from "framer-motion";
+import { AnimatePresence, LazyMotion } from "framer-motion";
 
 function MyApp({ Component, pageProps, router }) {
   const url = `https://kishans.in${router.route}`;
+  const loadFeatures = () =>
+    import("../utils/features").then((res) => res.default);
   return (
     <ChakraProvider theme={theme}>
-      <LazyMotion features={domAnimation} strict>
+      <LazyMotion features={loadFeatures}>
         <AnimatePresence
           exitBeforeEnter={true}
           initial={false}
