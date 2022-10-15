6b28959d24311ea0adcdaceb9528b1b94a986643
kishanhitk <kishansharma1231@gmail.com>
2022-10-15 10:56:26 +0530

    feat: add Next.js with tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <h1 className="text-3xl font-bold text-blue-500 underline">Hello world!</h1>
+  );
+};
+
+export default Home;
