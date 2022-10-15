cf623561bdabc369583e030420e608d9cf736566
kishanhitk <kishansharma1231@gmail.com>
2022-10-15 21:24:23 +0530

    feat: add projects page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,11 @@ import { useRouter } from "next/router";
 import { LazyMotion, AnimatePresence } from "framer-motion";
 import { useEffect } from "react";
 import * as ga from "../utils/analytics";
-import "@fontsource/inter/400.css";
 import "@fontsource/inter/300.css";
-import "@fontsource/inter/700.css";
+import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
