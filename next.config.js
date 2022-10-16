828d6c8e96fb15336a8e1999fc86806fcb6b9d2f
kishanhitk <kishansharma1231@gmail.com>
2022-10-16 16:09:20 +0530

    feat: remove preact as swcMinify produces lesser JS

--- next.config.js
+++ next.config.js
@@ -43,20 +43,6 @@ const nextConfig = {
       },
     ];
   },
-  webpack: (config, { dev, isServer }) => {
-    // Note, preact is only enabled for production builds (`next build`)
-    if (!dev && !isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      };
-    }
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
