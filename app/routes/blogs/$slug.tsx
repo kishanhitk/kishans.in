a04f62ec94140ccffd1724192d27e79ddcb68a72
kishanhitk <kishansharma1231@gmail.com>
2023-02-24 22:49:45 +0530

    feat: add routes

---
+++ app/routes/blogs/$slug.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Slug = () => {
+  return (
+    <div>Slug</div>
+  )
+}
+
+export default Slug
---
+++ app/routes/projects.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const projects = () => {
+  return (
+    <div>projects</div>
+  )
+}
+
+export default projects
