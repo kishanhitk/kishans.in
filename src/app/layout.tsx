2eb6d76431bd552308608fd7b0643f026345abba
kishanhitk <kishansharma1231@gmail.com>
2023-02-26 20:17:28 +0530

    feat: add microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { MainLayout } from "@layout";
 import "../styles/globals.css";
 import { Providers } from "./providers";
 import { Inter } from "@next/font/google";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,7 +15,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <head />
+      <head>
+        <Script
+          id="microsoft-clarity"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "g0b6ywg8xx");
+          `,
+          }}
+        />
+      </head>
       <body className="bg-white text-black dark:bg-[#1A1F2E] dark:text-white">
         <Providers>
           <MainLayout>{children}</MainLayout>
