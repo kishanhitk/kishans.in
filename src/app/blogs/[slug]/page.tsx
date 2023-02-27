a4c4f52d7eb461708aa334978c14457bd68c15fc
kishanhitk <kishansharma1231@gmail.com>
2023-02-27 09:36:11 +0530

    feat: add meta tags for blog

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,8 +1,39 @@
 import React from "react";
 import Image from "next/image";
 import { getAllPostByUsername, getPostBySlug } from "@functions/hashnode";
+import { Metadata } from "next/types";
 
 export const revalidate = 86400; // revalidate every day
+
+export async function generateMetadata({
+  params: { slug },
+}: any): Promise<Metadata> {
+  const { title, brief, coverImage } = await getPostBySlug(slug, "kishanhitk");
+  return {
+    title: title,
+    description: brief,
+    openGraph: {
+      title: title,
+      description: brief,
+      images: [
+        {
+          url: coverImage,
+          width: 800,
+          height: 600,
+          alt: "Og Image Alt",
+        },
+        {
+          url: coverImage,
+          width: 900,
+          height: 800,
+          alt: "Og Image Alt Second",
+        },
+        { url: coverImage },
+      ],
+      type: "article",
+    },
+  };
+}
 
 export async function generateStaticParams() {
   const posts = await getAllPostByUsername("kishanhitk");
