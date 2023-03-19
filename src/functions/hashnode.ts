e02e6cf9a0e0f11bc96114a61e7c32751cf74d19
kishanhitk <kishansharma1231@gmail.com>
2023-03-19 14:11:13 +0530

    feat: use fetch for gql calls

--- src/functions/hashnode.ts
+++ src/functions/hashnode.ts
@@ -1,71 +1,146 @@
-import client from "@apollo-client";
-import { gql } from "@apollo/client";
-import { HashnodePost, HashnodePostFull } from "@types";
+// import client from "@apollo-client";
+// import { gql } from "@apollo/client";
+// import { HashnodePost, HashnodePostFull } from "@types";
+
+// export const getAllPostByUsername = async (username: string) => {
+//   const { data } = await client.query({
+//     query: gql`
+//       query Posts($username: String!) {
+//         user(username: $username) {
+//           publication {
+//             posts(page: 0) {
+//               title
+//               brief
+//               slug
+//               cuid
+//             }
+//           }
+//         }
+//       }
+//     `,
+//     variables: {
+//       username,
+//     },
+//   });
+//   const posts: HashnodePost[] = data.user.publication.posts;
+//   return posts;
+// };
+
+// export const getPostBySlug = async (slug: string, hostname: string) => {
+//   const { data } = await client.query({
+//     query: gql`
+//       query Post($slug: String!, $hostname: String!) {
+//         post(slug: $slug, hostname: $hostname) {
+//           title
+//           dateAdded
+//           coverImage
+//           content
+//           slug
+//           brief
+//         }
+//       }
+//     `,
+//     variables: {
+//       slug,
+//       hostname,
+//     },
+//   });
+//   const post = data.post;
+//   return post as HashnodePostFull;
+// };
+// export const getPostMetadataBySlug = async (slug: string, hostname: string) => {
+//   const { data } = await client.query({
+//     query: gql`
+//       query Post($slug: String!, $hostname: String!) {
+//         post(slug: $slug, hostname: $hostname) {
+//           title
+//           dateAdded
+//           coverImage
+//           slug
+//           brief
+//         }
+//       }
+//     `,
+//     variables: {
+//       slug,
+//       hostname,
+//     },
+//   });
+//   const post = data.post;
+//   return post as HashnodePostFull;
+// };
+const API_URL = "https://api.hashnode.com"; // Replace with your GraphQL server URL
+
+async function fetchGraphQL(query: string, variables: any) {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  return await response.json();
+}
 
 export const getAllPostByUsername = async (username: string) => {
-  const { data } = await client.query({
-    query: gql`
-      query Posts($username: String!) {
-        user(username: $username) {
-          publication {
-            posts(page: 0) {
-              title
-              brief
-              slug
-              cuid
-            }
+  const query = `
+    query Posts($username: String!) {
+      user(username: $username) {
+        publication {
+          posts(page: 0) {
+            title
+            brief
+            slug
+            cuid
           }
         }
       }
-    `,
-    variables: {
-      username,
-    },
-  });
-  const posts: HashnodePost[] = data.user.publication.posts;
+    }
+  `;
+
+  const { data } = await fetchGraphQL(query, { username });
+  const posts = data.user.publication.posts;
   return posts;
 };
 
 export const getPostBySlug = async (slug: string, hostname: string) => {
-  const { data } = await client.query({
-    query: gql`
-      query Post($slug: String!, $hostname: String!) {
-        post(slug: $slug, hostname: $hostname) {
-          title
-          dateAdded
-          coverImage
-          content
-          slug
-          brief
-        }
+  const query = `
+    query Post($slug: String!, $hostname: String!) {
+      post(slug: $slug, hostname: $hostname) {
+        title
+        dateAdded
+        coverImage
+        content
+        slug
+        brief
       }
-    `,
-    variables: {
-      slug,
-      hostname,
-    },
-  });
+    }
+  `;
+
+  const { data } = await fetchGraphQL(query, { slug, hostname });
   const post = data.post;
-  return post as HashnodePostFull;
+  return post;
 };
+
 export const getPostMetadataBySlug = async (slug: string, hostname: string) => {
-  const { data } = await client.query({
-    query: gql`
-      query Post($slug: String!, $hostname: String!) {
-        post(slug: $slug, hostname: $hostname) {
-          title
-          dateAdded
-          coverImage
-          slug
-          brief
-        }
+  const query = `
+    query Post($slug: String!, $hostname: String!) {
+      post(slug: $slug, hostname: $hostname) {
+        title
+        dateAdded
+        coverImage
+        slug
+        brief
       }
-    `,
-    variables: {
-      slug,
-      hostname,
-    },
-  });
+    }
+  `;
+
+  const { data } = await fetchGraphQL(query, { slug, hostname });
   const post = data.post;
-  return post as HashnodePostFull;
+  return post;
 };
