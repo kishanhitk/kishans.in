24113b4b9f06f689979c2c09965c75915f2580cb
kishanhitk <kishansharma1231@gmail.com>
2023-08-16 16:54:04 +0530

    chore: restructuring

--- src/app/(general)/not-found.tsx
+++ src/app/(general)/not-found.tsx
@@ -4,13 +4,11 @@ import { Four04Image } from "@components/Four04Image";
 
 export default function Four04Page() {
   return (
-    <>
-      <div className="flex flex-col">
-        <Four04Image />
-        <Link href="/" className="mx-auto" passHref replace={true}>
-          Go Home
-        </Link>
-      </div>
-    </>
+    <div className="flex flex-col">
+      <Four04Image />
+      <Link href="/" className="mx-auto" passHref replace={true}>
+        Go Home
+      </Link>
+    </div>
   );
 }
