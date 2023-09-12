04348d9619d34b2dca4c1d89630de3c2c64a3fff
kishanhitk <kishansharma1231@gmail.com>
2023-09-12 16:09:56 +0530

    feat: add runtime-edge

--- src/app/(general)/[...not-found]/page.tsx
+++ src/app/(general)/[...not-found]/page.tsx
@@ -1,7 +1,7 @@
 // Workaround for rendering not-found page when multiple using route groups. Ref: https://github.com/vercel/next.js/discussions/50034
 
+export const runtime = "edge";
 import { notFound } from "next/navigation";
-
 export default function NotFoundDummy() {
   notFound();
 }
--- src/app/(general)/blogs/[slug]/page.tsx
+++ src/app/(general)/blogs/[slug]/page.tsx
@@ -1,3 +1,4 @@
+export const runtime = "edge";
 import React from "react";
 import Image from "next/image";
 import {
