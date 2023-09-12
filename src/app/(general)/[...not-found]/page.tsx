783546eee77033ff410d3e5cc58ae989da16cbce
kishanhitk <kishansharma1231@gmail.com>
2023-09-12 16:21:46 +0530

    feat: remove unncessary routes

--- src/app/(general)/[...not-found]/page.tsx
+++
@@ -1,7 +0,0 @@
-// Workaround for rendering not-found page when multiple using route groups. Ref: https://github.com/vercel/next.js/discussions/50034
-
-import { notFound } from "next/navigation";
-
-export default function NotFoundDummy() {
-  notFound();
-}
