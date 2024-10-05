1d11333c77001fb7e726c5ea9dda23de06f6894e
kishanhitk <kishansharma1231@gmail.com>
2024-10-05 20:22:02 +0530

    wip: homepage

---
+++ src/components/home/Homepage.tsx
@@ -0,0 +1,46 @@
+import { motion, type Variant } from "framer-motion";
+import { Suspense } from "react";
+import { SocialLinks } from "../shared/SocialLinks";
+import Paragraph from "../shared/Paragraph";
+
+const fadeInUp = {
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0 },
+};
+
+const staggerChildren = {
+  animate: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Home = () => {
+  return (
+
+  <>
+      <motion.h1
+      className="text-4xl font-bold dark:text-white md:text-5xl"
+      variants={fadeInUp}
+    >
+      Hi, I&apos;m Kishan
+    </motion.h1>
+    <motion.div variants={fadeInUp}>
+      <Paragraph>
+        I&apos;m a self-taught full-stack developer hailing from India who
+        loves hacking cool stuff! <strong>Next.js</strong> and{" "}
+        <strong>Remix.run</strong> are my go-to weapons, but sometimes, I
+        dive into the world of NFT smart contracts with{" "}
+        <strong>Scilla</strong> on <strong>Zilliqa</strong>. You could say
+        I&apos;m a jack of all trades, master of fun. 😄
+      </Paragraph>
+    </motion.div>
+    <motion.div variants={fadeInUp}>
+      <SocialLinks />
+    </motion.div></>
+
+  );
+};
+
+export default Home;
---
+++ src/functions/hashnode.ts
@@ -0,0 +1,103 @@
+import type { HashnodePost, HashnodePostFull } from "../types/hashnode";
+
+
+const API_URL = "https://gql.hashnode.com";
+
+async function fetchGraphQL<T = any>(
+  query: string,
+  variables: object
+): Promise<T> {
+  const response = await fetch(API_URL, {
+
+    cache:"force-cache",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  return await response.json();
+}
+
+export const getAllPostByUsername = async (
+  host: string
+): Promise<HashnodePost[]> => {
+  const query = /* GraphQL */ `
+    query Publication($host: String!) {
+      publication(host: $host) {
+        posts(first: 11) {
+          edges {
+            node {
+              title
+              brief
+              cuid
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const { data } = await fetchGraphQL(query, { host });
+  const posts: HashnodePost[] = data.publication.posts.edges.map(
+    (post: any) => post.node
+  );
+  return posts;
+};
+
+export const getPostBySlug = async (
+  slug: string,
+  hostname: string
+): Promise<HashnodePostFull> => {
+  const query = /* GraphQL */ `
+    query Publication($slug: String!, $hostname: String!) {
+      publication(host: $hostname) {
+        post(slug: $slug) {
+          title
+          brief
+          coverImage {
+            url
+          }
+          updatedAt
+          content {
+            markdown
+          }
+        }
+      }
+    }
+  `;
+  const { data } = await fetchGraphQL(query, { slug, hostname });
+  const post: HashnodePostFull = data.publication.post;
+  return post;
+};
+
+export const getPostMetadataBySlug = async (
+  slug: string,
+  hostname: string
+): Promise<HashnodePostFull> => {
+  const query = /* GraphQL */ `
+    query Publication($slug: String!, $hostname: String!) {
+      publication(host: $hostname) {
+        post(slug: $slug) {
+          title
+          updatedAt
+          coverImage {
+            url
+          }
+          slug
+          brief
+        }
+      }
+    }
+  `;
+
+  const { data } = await fetchGraphQL(query, { slug, hostname });
+  const post: HashnodePostFull = data.publication.post;
+  return post;
+};
