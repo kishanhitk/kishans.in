67267fe7b543c6ae58c8c9c6f2c67f80d710ccfc
kishanhitk <kishansharma1231@gmail.com>
2024-10-23 20:22:39 +0530

    feat: try new color

--- src/components/shared/Paragraph.tsx
+++ src/components/shared/Paragraph.tsx
@@ -7,7 +7,7 @@ type ParagraphProps = {
 
 const Paragraph: React.FC<ParagraphProps> = ({ children, className }) => (
   <p
-    className={`text-md font-normal leading-7 text-gray-700 dark:text-gray-200 ${className}}`}
+    className={`text-lg leading-8 text-gray-700 dark:text-gray-200 ${className}}`}
   >
     {children}
   </p>
