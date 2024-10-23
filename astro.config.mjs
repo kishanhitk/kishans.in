3ee290c280983c6f47eb1539cff65bbe1c4776d3
kishanhitk <kishansharma1231@gmail.com>
2024-10-23 22:54:37 +0530

    feat: add vercel integeration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,29 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
+
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
   ],
+
+  output: "static",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+    imageService: true,
+    imagesConfig: {
+      domains: ["images.unsplash.com"],
+      sizes: [320, 640, 1280],
+    },
+    isr: {
+      expiration: 60 * 60 * 24, // 1 day
+    },
+  }),
 });
