a11cd20b6ba9341de16493c0f4e373248899fb52
kishanhitk <kishansharma1231@gmail.com>
2025-10-25 16:53:00 +0530

    chore: update astro configuration, package dependencies, and add favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@
 import react from '@astrojs/react'
 
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/static'
+import vercel from '@astrojs/vercel'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
